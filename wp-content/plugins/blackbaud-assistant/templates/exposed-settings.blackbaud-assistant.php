128657cad11cd3e7588ad72e66289ff17241f6ca
Lucas Cobb <lucasdcobb@gmail.com>
2017-12-24 14:16:49 +0000

    lc: removing unused plugins

--- wp-content/plugins/blackbaud-assistant/templates/exposed-settings.blackbaud-assistant.php
+++
@@ -1,5 +0,0 @@
-<script id="blackbaud-settings-<?php echo $data['alias']; ?>">
-    window.BBWP = window.BBWP || {};
-    window.BBWP.plugins = window.BBWP.plugins || {};
-    window.BBWP.plugins["<?php echo $data['alias']; ?>"] = <?php echo json_encode($data['settings'], true); ?>;
-</script>
